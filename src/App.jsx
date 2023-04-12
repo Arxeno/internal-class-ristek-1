aaa37d8aedc6d91fc8f3e330f03752e156c002e6
Raisyam <rmfbkw04@gmail.com>
2023-04-12 23:54:29 +0700

    start creating expense detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import './App.scss';
 import Home from './pages/Home';
+import ExpenseDetail from './pages/ExpenseDetail';
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <ExpenseDetail />
     </div>
   );
 }
